1e5552ca5918c0764b1d10a15d0226ddbecdc722
PauloCesar26 <pc.cordeirolima@gmail.com>
2026-01-06 21:26:28 -0300

    Adding show password in login area

--- controle-estoque/src/pages/login-admin/script-login/event-login.js
+++ controle-estoque/src/pages/login-admin/script-login/event-login.js
@@ -3,11 +3,17 @@ import { loginEmailAndPassword } from "./auth-firebase.js";
 export function loginAdmin(){
     const adminEmail = document.getElementById("email");
     const adminPassword = document.getElementById("password");
+    const btnPassword = document.querySelector(".show-password");
 
     const btnLogin = document.getElementById("btn-login");
 
     btnLogin.addEventListener("click", (e) => {
         e.preventDefault();
         loginEmailAndPassword(adminEmail, adminPassword);
+    });
+
+    btnPassword.addEventListener("click", () => {
+        const isPassword = adminPassword.type === "password";
+        adminPassword.type = isPassword ? "text" : "password";
     });
 }
